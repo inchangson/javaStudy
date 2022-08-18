6f8b3fa568938927785a05bcd4a3390655ef9936
inchang <3721cha21.11@gmail.com>
2022-08-18 15:56:21 +0900

    Feat : add using abstract and interface case

---
+++ src/oop/inheritance/abstract_and_interface/Earth.java
@@ -0,0 +1,9 @@
+package oop.inheritance.abstract_and_interface;
+
+public interface Earth {
+
+    void breathe();
+    default void grow(){
+
+    }
+}
---
+++ src/oop/inheritance/abstract_and_interface/Human.java
@@ -0,0 +1,8 @@
+package oop.inheritance.abstract_and_interface;
+
+abstract public class Human implements Earth{
+    int age;
+    int status;
+    abstract void envy();
+
+}
